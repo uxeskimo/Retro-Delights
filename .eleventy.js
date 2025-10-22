2da7c8991055fab02786922016cc364c7371f443
“Lee <uxeskimo@gmail.com>
2025-10-22 19:53:20 +0100

    change svg location

--- .eleventy.js
+++ .eleventy.js
@@ -20,6 +20,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/fonts");
   eleventyConfig.addPassthroughCopy("./src/img");
+  eleventyConfig.addPassthroughCopy("./src/svg");
   eleventyConfig.addPassthroughCopy("./src/favicon.png");
 
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
